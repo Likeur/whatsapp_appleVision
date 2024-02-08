8e8b4aa97e243d423e0358a7f981cce7fc53317c
Likeur <jacksimba28@gmail.com>
2024-02-08 03:13:07 +0200

    adding chatlist and chatpreview

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ChatListComponent } from '../../components/chat-list/chat-list.component';
+import { ChatpreviewComponent } from '../../components/chatpreview/chatpreview.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ChatListComponent, ChatpreviewComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
