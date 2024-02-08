57104ddf7a87c3e7a7d613a64929e0ea10d2376d
Likeur <jacksimba28@gmail.com>
2024-02-08 02:38:25 +0200

    config plus add component and layout

---
+++ src/app/components/chat-list/chat-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-list.component.html',
+  styles: ``
+})
+export class ChatListComponent {
+
+}
---
+++ src/app/components/chatpreview/chatpreview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatpreview',
+  standalone: true,
+  imports: [],
+  templateUrl: './chatpreview.component.html',
+  styles: ``
+})
+export class ChatpreviewComponent {
+
+}
